c199d1e5daf04e6bfd5aaeacee739fe30120373e
Matthew <matthewbrown595@gmail.com>
2024-04-05 14:01:47 +0100

    name adjustments

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 }
 
 # Create S3 bucket for Terraform state
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "team-dkr-terraform-state-bucket"
+resource "aws_s3_bucket" "matt_terraform_state_bucket" {
+  bucket = "matt-team-dkr-terraform-state-bucket"
   acl    = "private"
   tags   = {
     "cohort-dkr" = "mar-2024-cloud-engineering-dkr"
@@ -15,8 +15,8 @@ resource "aws_s3_bucket" "terraform_state_bucket" {
 }
 
 # Create ECR Repository
-resource "aws_ecr_repository" "team_dkr_repo" {
-  name = "team-dkr-repo"
+resource "aws_ecr_repository" "matt_team_dkr_repo" {
+  name = "matt-team-dkr-repo"
   tags = {
     "cohort-dkr" = "mar-2024-cloud-engineering-dkr"
   }
@@ -24,7 +24,7 @@ resource "aws_ecr_repository" "team_dkr_repo" {
 
 # Create Elastic Beanstalk Application
 resource "aws_elastic_beanstalk_application" "team_dkr_app" {
-  name = "team-dkr-app"
+  name = "matt-team-dkr-app"
   tags = {
     "cohort-dkr" = "mar-2024-cloud-engineering-dkr"
   }
@@ -32,21 +32,21 @@ resource "aws_elastic_beanstalk_application" "team_dkr_app" {
 
 # Create Elastic Beanstalk Environment
 resource "aws_elastic_beanstalk_environment" "team_dkr_eb_env" {
-  name                = "team-dkr-eb-env"
+  name                = "matt-team-dkr-eb-env"
   application         = aws_elastic_beanstalk_application.team_dkr_app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.3.3 running Python 3.9" # Example solution stack
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "DATABASE_URL"
-    value     = aws_db_instance.team_dkr_db_instance.address
+    value     = aws_db_instance.matt_team_dkr_db_instance.address
   }
 
   # Link to the S3 bucket for application artifacts
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
     name      = "S3_BUCKET_NAME"
-    value     = aws_s3_bucket.team_dkr_app_bucket.bucket
+    value     = aws_s3_bucket.matt_team_dkr_app_bucket.bucket
   }
 
   tags = {
@@ -55,19 +55,21 @@ resource "aws_elastic_beanstalk_environment" "team_dkr_eb_env" {
 }
 
 # Create RDS PostgreSQL Database
-resource "aws_db_instance" "team_dkr_db_instance" {
+resource "aws_db_instance" "matt_team_dkr_db_instance" {
   engine            = "postgres"
   instance_class    = "db.t2.micro"
   allocated_storage = 20
-  identifier        = "team-dkr-db-instance"
+  identifier        = "matt-team-dkr-db-instance"
+  username = "my_username"
+  password = "my_password"
   tags = {
     "cohort-dkr" = "mar-2024-cloud-engineering-dkr"
   }
 }
 
 # Create S3 Bucket for Elastic Beanstalk Application
-resource "aws_s3_bucket" "team_dkr_app_bucket" {
-  bucket = "team-dkr-app-bucket"
+resource "aws_s3_bucket" "matt_team_dkr_app_bucket" {
+  bucket = "matt-team-dkr-app-bucket"
   acl    = "private"
 
   tags = {
